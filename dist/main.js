9888745c2dce7457f885ba4a0368953c469d1d82
IgorBanjac91 <igor.banjac91@gmail.com>
2021-06-05 10:32:42 +0200

    set up webpack and add dev tools and plugins

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+console.log("ok");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJzaXRlLy4vc3JjL2luZGV4LmpzIl0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiJBQUFBQSxRQUFRQyxJQUFJIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIm9rXCIpIl0sInNvdXJjZVJvb3QiOiIifQ==
