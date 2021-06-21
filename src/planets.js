31014df083aee12a249ac221df1d08a56699da5b
IgorBanjac91 <igor.banjac91@gmail.com>
2021-06-21 15:17:13 +0200

    add css and html fro carousel and start js

--- src/planets.js
+++ src/planets.js
@@ -9,9 +9,19 @@ import Tablet from "./images/screenshots/planets-tablet.png";
 import Mobile from "./images/screenshots/planets-mobile.png";
 
 
-let imageContainer = document.querySelector(".images-container");
+// ---------------------- Carousel ----------------------
 
-console.log(imageContainer)
+let track = document.querySelector(".carousel__track")
+let trackContainer = document.querySelector(".carousel__track-container");
+let leftButton = document.querySelector(".carousel__button--left")
+let rightButton = document.querySelector(".carousel__button--right")
+let carouselNav = document.querySelector(".carousel__nav");
+let indicators = Array.from(carouselNav.children);
+let slides = Array.from(track.children);
+let slide1 = document.querySelector(".carousel__slide-1");
+let slide2 = document.querySelector(".carousel__slide-2");
+let slide3 = document.querySelector(".carousel__slide-3");
+
 
 const desktopScreenshot = new Image();
 const tableScreenshot = new Image();
@@ -21,12 +31,27 @@ desktopScreenshot.src = Desktop;
 tableScreenshot.src = Tablet;
 mobileScreenshot.src = Mobile;
 
-imageContainer.appendChild(desktopScreenshot);
-imageContainer.appendChild(tableScreenshot);
-imageContainer.appendChild(mobileScreenshot);
+slide1.appendChild(mobileScreenshot);
+slide3.appendChild(desktopScreenshot);
+slide2.appendChild(tableScreenshot);
+
+const slide1Width = slide1.getBoundingClientRect().width;
+const slide2Width = slide2.getBoundingClientRect().width;
+const slide3Width = slide3.getBoundingClientRect().width;
+
+slide1.style.left = `${slide1Width * 0}px`;
+slide2.style.left = `${slide1Width * 1}px`;
+slide3.style.left = `${slide2Width * 2}px`;
 
 
-// Menu 
+
+
+
+
+
+
+
+// ---------------------- Nav ----------------------
 
 let menuBtn = document.querySelector(".menu-btn");
 let menu = document.querySelector("#menu");
