0e5d5a7133078f584a0292c5f26a81e7771bf1bf
IgorBanjac91 <igor.banjac91@gmail.com>
2021-06-22 14:30:08 +0200

    add medium font weight, add home link, and assign correct redirection for links

--- src/planets.js
+++ src/planets.js
@@ -178,3 +178,13 @@ menuBtn.addEventListener("click", function toggleMenu(e) {
   e.preventDefault();
   menu.classList.toggle("show-menu");
 });
+
+// ---------------------- Nav Links ----------------------
+
+let aboutLink = document.querySelector(`[href="#about"]`);
+let projectsLink = document.querySelector(`[href="#projects"]`);
+let contactLink = document.querySelector(`[href="#contact"]`);
+
+aboutLink.href = "/index.html#about";
+projectsLink.href = "/index.html#projects";
+contactLink.href = "/index.html#contact";
